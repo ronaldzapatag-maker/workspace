075aad3e1fbea460c0e1ed40ea99d46dd197d5d5
Songohan <ronaldzapatag@ielacandelariamedellin.edu.co>
2026-03-17 21:04:40 +0000

    javascriptura

---
+++ adivinanza.js
@@ -0,0 +1,11 @@
+let a = 17;
+let b = 16;
+
+if(a ==b){
+    console.log("adivinaste")
+}else if(a>b){
+    console.log("muy grande");
+} else if(a<b){
+    console.log("muy pequeño");
+}
+
---
+++ ciclos.js
@@ -0,0 +1,17 @@
+let a = 1;
+while (a<100){
+    console.log(a);
+    a = a+1;
+}
+
+let b = 2;
+while (b<101){
+    console.log(b);
+    b = b+2;
+}
+
+let c = 3;
+while (c<101){
+    console.log(c);
+    c = c+2;
+}
